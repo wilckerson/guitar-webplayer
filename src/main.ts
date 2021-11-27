96da93b28abad09daa498d70d089ff7f6fa201b4
Wilckerson <wilckerson@gmail.com>
2021-11-27 01:19:24 -0800

    Working in the main layout

--- src/main.ts
+++ src/main.ts
@@ -1,76 +1,79 @@
-import "./style.css";
-var debugText = document.getElementById("debugText");
-var touchIndicators = [
-  document.getElementById("touchIndicator1"),
-  document.getElementById("touchIndicator2"),
-  document.getElementById("touchIndicator3"),
-  document.getElementById("touchIndicator4"),
-];
+import "./styles/index.css";
+import "./styles/header.css";
+import "./styles/scale.css";
 
-document.addEventListener("touchstart", handleTouchStart, false);
-document.addEventListener("touchmove", handleTouchMove, false);
-document.addEventListener("touchcancel", handleTouchCancel, false);
-document.addEventListener("touchend", handleTouchEnd, false);
+// var debugText = document.getElementById("debugText");
+// var touchIndicators = [
+//   document.getElementById("touchIndicator1"),
+//   document.getElementById("touchIndicator2"),
+//   document.getElementById("touchIndicator3"),
+//   document.getElementById("touchIndicator4"),
+// ];
 
-function handleTouchStart(ev: TouchEvent) {
-  //console.log("handleTouchStart", ev);
-  for (let index = 0; index < ev.touches.length; index++) {
-    const currentTouch = ev.touches[index];
-    updateTouchIndicatorPosition(
-      index,
-      currentTouch.clientX,
-      currentTouch.clientY
-    );
-  }
+// document.addEventListener("touchstart", handleTouchStart, false);
+// document.addEventListener("touchmove", handleTouchMove, false);
+// document.addEventListener("touchcancel", handleTouchCancel, false);
+// document.addEventListener("touchend", handleTouchEnd, false);
 
-  debugTouchIndicators();
-}
+// function handleTouchStart(ev: TouchEvent) {
+//   //console.log("handleTouchStart", ev);
+//   for (let index = 0; index < ev.touches.length; index++) {
+//     const currentTouch = ev.touches[index];
+//     updateTouchIndicatorPosition(
+//       index,
+//       currentTouch.clientX,
+//       currentTouch.clientY
+//     );
+//   }
 
-function handleTouchMove(ev: TouchEvent) {
-  //console.log("handleTouchMove",ev);
-  for (let index = 0; index < ev.touches.length; index++) {
-    const currentTouch = ev.touches[index];
-    updateTouchIndicatorPosition(
-      index,
-      currentTouch.clientX,
-      currentTouch.clientY
-    );
-  }
+//   debugTouchIndicators();
+// }
 
-  debugTouchIndicators();
-}
+// function handleTouchMove(ev: TouchEvent) {
+//   //console.log("handleTouchMove",ev);
+//   for (let index = 0; index < ev.touches.length; index++) {
+//     const currentTouch = ev.touches[index];
+//     updateTouchIndicatorPosition(
+//       index,
+//       currentTouch.clientX,
+//       currentTouch.clientY
+//     );
+//   }
 
-function handleTouchCancel(ev: TouchEvent) {
-  //console.log("handleTouchCancel",ev);
-}
+//   debugTouchIndicators();
+// }
 
-function handleTouchEnd(ev: TouchEvent) {
-  //console.log("handleTouchCancel",ev);
-}
+// function handleTouchCancel(ev: TouchEvent) {
+//   //console.log("handleTouchCancel",ev);
+// }
 
-function updateTouchIndicatorPosition(index: number, x: number, y: number) {
-  if (index >= touchIndicators.length) {
-    return;
-  }
+// function handleTouchEnd(ev: TouchEvent) {
+//   //console.log("handleTouchCancel",ev);
+// }
 
-  touchIndicators[index].style.left = x + "px";
-  touchIndicators[index].style.top = y + "px";
-}
+// function updateTouchIndicatorPosition(index: number, x: number, y: number) {
+//   if (index >= touchIndicators.length) {
+//     return;
+//   }
 
-function debugTouchIndicators() {
-  if (!debugText) {
-    return;
-  }
-  var debug = "";
-  for (let index = 0; index < touchIndicators.length; index++) {
-    const touchIndicator = touchIndicators[index];
-    debug +=
-      "x: " +
-      parseInt(touchIndicator?.style.left || "0") +
-      " y: " +
-      parseInt(touchIndicator?.style.top || "0");
-    debug += "<br/>";
-  }
+//   touchIndicators[index].style.left = x + "px";
+//   touchIndicators[index].style.top = y + "px";
+// }
 
-  debugText.innerHTML = debug;
-}
+// function debugTouchIndicators() {
+//   if (!debugText) {
+//     return;
+//   }
+//   var debug = "";
+//   for (let index = 0; index < touchIndicators.length; index++) {
+//     const touchIndicator = touchIndicators[index];
+//     debug +=
+//       "x: " +
+//       parseInt(touchIndicator?.style.left || "0") +
+//       " y: " +
+//       parseInt(touchIndicator?.style.top || "0");
+//     debug += "<br/>";
+//   }
+
+//   debugText.innerHTML = debug;
+// }
